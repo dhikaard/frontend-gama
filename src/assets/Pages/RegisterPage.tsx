a9e3881b84a4324a1d115a3bfb18be3c0d33ccaf
dhikaard <yusufardian41@gmail.com>
2024-06-02 21:53:19 +0700

    .

--- src/assets/Pages/RegisterPage.tsx
+++ src/assets/Pages/RegisterPage.tsx
@@ -15,13 +15,15 @@ import { IconAlertTriangle } from "@tabler/icons-react";
 import { useForm, isEmail, isNotEmpty, hasLength } from "@mantine/form";
 import { useNavigate } from 'react-router-dom';
 import LogoGama from "../img/logo.svg";
-
 // Import Icon
 import { GoogleButton } from "../Components/GoogleButton";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function RegisterPage() {
+  const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
+
   const form = useForm({
     initialValues: {
       full_name: "",
@@ -30,7 +32,6 @@ function RegisterPage() {
       email: "",
       password: "",
     },
-
     validate: {
       full_name: hasLength({ min: 2, max: 45 }),
       address: isNotEmpty(),
@@ -40,12 +41,37 @@ function RegisterPage() {
     },
   });
 
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/v1/auth")
+      .then((data) => {
+        setUrl(data.data.url);
+      });
+  }, []);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (values) => {
+
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/v1/auth/register", values);
+      console.log("Registrasi berhasil:", response.data);
+      navigate('/masuk');
+    } catch (error) {
+      if (error.response) {
+        setError(error.response.data.errors || "Registrasi gagal. Silakan coba lagi.");
+      } else if (error.request) {
+        setError("Tidak ada respon dari server. Silakan coba lagi.");
+      } else {
+        setError("Terjadi kesalahan dalam mengatur permintaan. Silakan coba lagi.");
+      }
+    }
+  };
+
   return (
     <div className="register">
       <Flex className="register-hero" direction="column" align="center">
         <img src={LogoGama} alt="Logo Gama" className="gama-register" />
       </Flex>
-
       <Flex my="1.5rem" direction="column" gap="8" align="center">
         <Title className="register-title" order={3}>
           Daftar sebagai Pengguna
@@ -54,22 +80,38 @@ function RegisterPage() {
           Daftar dan bergabunglah menjadi masyarakat peduli lingkungan.
         </Text>
       </Flex>
-
       <Box
         component="form"
         maw={340}
         mx="auto"
         className="form-register"
-        onSubmit={form.onSubmit(() => {})}
+        onSubmit={form.onSubmit(handleSubmit)}
       >
+        {error && (
+          <Alert
+            className="alert"
+            mb="sm"
+            variant="light"
+            color="red"
+            radius="md"
+            withCloseButton
+            title="Perhatian!"
+            icon={<IconAlertTriangle />}
+            onClose={() => setError("")}
+            lh="1rem"
+          >
+            {error.phone_number}
+          </Alert>
+        )}
 
         <TextInput
           className="register-input"
           label="Nama Lengkap"
           withAsterisk
           radius="xl"
-          placeholder="cth. Ferdyan Steevandio"         
-          {...form.getInputProps("nama_lengkap")}
+          placeholder="cth. Ferdyan Steevandio"
+          {...form.getInputProps("full_name")}
+          placeholder="cth. Ferdyan Steevandio"
         />
 
         <TextInput
@@ -81,7 +123,6 @@ function RegisterPage() {
           placeholder="cth. Ngaliyan, Kota Semarang"
           {...form.getInputProps("address")}
         />
-
         <TextInput
           className="register-input"
           label="No. WhatsApp"
@@ -91,7 +132,6 @@ function RegisterPage() {
           placeholder="cth. 081234567890"
           {...form.getInputProps("phone_number")}
         />
-
         <TextInput
           className="register-input"
           label="Email"
@@ -101,7 +141,6 @@ function RegisterPage() {
           placeholder="cth. user@gmail.com"
           {...form.getInputProps("email")}
         />
-
         <PasswordInput
           className="register-input"
           label="Buat kata sandi"
@@ -111,16 +150,17 @@ function RegisterPage() {
           placeholder="cth. #123Ri&8"
           {...form.getInputProps("password")}
         />
-
         <Group justify="center" mt="md">
           <Button fullWidth radius="xl" color="#416835" type="submit">
             Daftar sebagai Pengguna
           </Button>
         </Group>
-
         <Divider label="atau" labelPosition="center" my="xs" />
 
         <GoogleButton className="btn-google" fullWidth radius="xl">
+          <Anchor<"a"> href={url} ta={"center"} size="sm" my={"sm"}>
+            Daftar dengan Google
+          </Anchor>
           Daftar dengan Google
         </GoogleButton>
 
