0a6b0158a83c05badc8ecf6c4a241e51f0940b96
dhikaard <yusufardian41@gmail.com>
2024-06-02 11:06:14 +0700

    Add AccountPage

--- src/assets/Pages/AccountPage.tsx
+++ src/assets/Pages/AccountPage.tsx
@@ -11,97 +11,21 @@ import {
   Card,
   Stack
 } from "@mantine/core";
-import {
-  IconMapPin,
-  IconPencil,
-  IconAlertTriangle,
-  IconPackage,
-  IconNotebook,
-  IconBottle,
-  IconPlus,
-  IconMinus,
-} from "@tabler/icons-react";
-import { useNavigate } from "react-router-dom";
-import * as Yup from "yup";
-
-const dataLocal = [
-  {
-    bank_sampah: "Bank Sampah 1",
-    item_sampah: "bottle ,cardboard, paper",
-    location: "Suite 55",
-  },
-  {
-    bank_sampah: "Bank Sampah 2",
-    item_sampah: "paper, bottle",
-    location: "PO Box 41613",
-  },
-  {
-    bank_sampah: "Bank Sampah 3",
-    item_sampah: "bottle",
-    location: "PO Box 50058",
-  },
-  { bank_sampah: "Bank Sampah 4", item_sampah: "paper", location: "8th Floor" },
-  {
-    bank_sampah: "Bank Sampah 5",
-    item_sampah: "paper, cardboard",
-    location: "PO Box 57936",
-  },
-];
-
-const validationSchema = Yup.object().shape({
-  bankSampah: Yup.string().required(
-    "Silahkan untuk memilih bank sampah terlebih dahulu"
-  ),
-  items: Yup.object()
-    .shape({
-      BotolPlastik: Yup.number()
-        .min(1, "Minimal salah satu item harus bernilai 1")
-        .required(),
-      Kardus: Yup.number()
-        .min(1, "Minimal salah satu item harus bernilai 1")
-        .required(),
-      Kertas: Yup.number()
-        .min(1, "Minimal salah satu item harus bernilai 1")
-        .required(),
-    })
-    .test(
-      "oneOfRequired",
-      "Minimal salah satu item harus bernilai 1",
-      (value) => {
-        return Object.values(value).some((val) => val >= 1);
-      }
-    ),
-});
-
 function ExchangePage() {
-  const navigate = useNavigate();
 
   const [currentDate, setCurrentDate] = useState("");
-  const [bankSampah, setBankSampah] = useState<any[]>([]);
-  const [searchValue, setSearchValue] = useState("");
-  const [selectedBankSampah, setSelectedBankSampah] = useState("");
-  const [errors, setErrors] = useState<any>({});
-  const [showAlert, setShowAlert] = useState(true);
 
   useEffect(() => {
     const today = new Date();
     const options = { day: "2-digit", month: "2-digit", year: "numeric" };
     setCurrentDate(today.toLocaleDateString("id-ID", options));
-    setBankSampah(dataLocal);
   }, []);
-
-  const IconMap = (
-    <IconMapPin
-      stroke={2}
-      style={{ width: rem("1.25rem"), height: rem("1.25rem") }}
-    />
-  );
 
   return (
     <div className="penukaran-sampah">
       <Container display="flex" bg="#F8F9FA" h="3.75rem" className="header">
         <Title order={6} ta="center" lh="1.125rem" fw={600} size="xl">
-          Penukaran
+          Profil Pengguna
         </Title>
       </Container>
       <Flex className="main" direction="column" mt="1.5rem" mb="5rem">
